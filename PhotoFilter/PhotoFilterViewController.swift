01367ca0d31fc57555057e64a780929ab8dd959c
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-11-05 12:24:14 -0600

    CIFilter added

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -12,30 +12,55 @@ class PhotoFilterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     @IBAction func choosePhoto(_ sender: Any) {
+        presentImagePickerController()
     }
     
     @IBAction func savePhoto(_ sender: Any) {
     }
     
     @IBAction func changeBrightness(_ sender: Any) {
+        updateImage()
     }
     
     @IBAction func changeContrast(_ sender: Any) {
+        updateImage()
     }
     
     @IBAction func changeSaturation(_ sender: Any) {
+        updateImage()
     }
     
     private func updateImage() {
         // Filter
+        guard let originalImage = originalImage else { return }
         
         // Put
-        imageView.image = originalImage
+        imageView.image = image(byFiltering: originalImage)
     }
+    
+    private func image(byFiltering image: UIImage) -> UIImage {
+        
+        guard let cgImage = image.cgImage else { return image }
+        // Check why ???
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
+        filter.setValue(brighnessSlider.value, forKey: kCIInputBrightnessKey)
+        filter.setValue(contrastSlider.value, forKey: kCIInputContrastKey)
+        filter.setValue(saturationSlider.value, forKey: kCIInputSaturationKey)
+        
+        guard let outputCIImage = filter.outputImage else { return image }
+        guard let outputCGImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else { return image }
+        
+        
+        return UIImage(cgImage: outputCGImage)
+    }
+    
+    private let filter = CIFilter(name: "CIColorControls")!
+    private let context = CIContext(options: nil)
     
     private var originalImage: UIImage? {
         didSet { updateImage() }
@@ -49,7 +74,7 @@ class PhotoFilterViewController: UIViewController {
 
 extension PhotoFilterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    // Need to add
+    // Need to add UINavigationControllerDelegate
     private func presentImagePickerController() {
         guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
             NSLog("Photo Libarary is unavailable")
